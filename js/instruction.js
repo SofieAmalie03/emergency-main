20c604e9a93cedc3c7ff37ea220852f9236fe2c3
SofieAmalie03 <sahc03@icloud.com>
2025-11-06 13:07:46 +0100

    updates .js

---
+++ js/instruction.js
@@ -0,0 +1,15 @@
+document
+  .querySelector(".info-graphic button:nth-of-type(1)")
+  .addEventListener("click", function () {
+    console.log("BMO button clicked");
+  });
+document
+  .querySelector(".info-graphic button:nth-of-type(2)")
+  .addEventListener("click", function () {
+    console.log("KRISE button clicked");
+  });
+document
+  .querySelector(".info-graphic button:nth-of-type(3)")
+  .addEventListener("click", function () {
+    console.log("VEJLEDNING button clicked");
+  });
